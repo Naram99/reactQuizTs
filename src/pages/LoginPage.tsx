6813972ee8a0c13c8008d80abc78680e973173b1
Naram99 <131804895+Naram99@users.noreply.github.com>
2025-01-06 23:10:20 +0100

    Router added to app.tsx
    Router added, login route works

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -11,9 +11,16 @@ export default function LoginPage() {
         <div className="loginWrapper">
             <div className="loginFormCt">
                 <form action="" id="loginForm">
+                    <label htmlFor="userName">Felhasználónév</label>
                     <input type="text" name="userName" id="userNameInput" />
+                    <label htmlFor="userName">Jelszó</label>
                     <input type="password" name="userPw" id="userPwInput" />
-                    {register && <input type="password" name="userPwCheck" id="userPwCheckInput" />}
+                    {register && (
+                        <>
+                            <label htmlFor="userName">Jelszó újra</label>
+                            <input type="password" name="userPwCheck" id="userPwCheckInput" />
+                        </>
+                    )}
                 </form>
                 <button onClick={toggleRegister}>{register ? "Bejelentkezés" : "Regisztráció"}</button>
             </div>
